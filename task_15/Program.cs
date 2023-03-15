6f92af1e9b3c0cc9a5eb8b8a8ed016cfb5c4ef2d
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 19:16:07 +0300

    Задача 15 - решение

---
+++ task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA;
+
+//Опровобала конструкцию циклического ввода переменной из семинара
+while (true)
+{
+    Console.Write("Введите номер дня недели: ");
+    if (int.TryParse(Console.ReadLine(), out numberA) & numberA > 0 & numberA < 8)
+        break;
+    Console.WriteLine("Ошибка ввода, введите число от 1 до 7");
+}
+string result;
+
+if (numberA == 6 || numberA == 7)
+{
+    result = "да";
+}
+
+else result = "нет";
+
+Console.WriteLine($"{numberA} -> {result}");
