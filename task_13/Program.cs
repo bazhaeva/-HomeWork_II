9f3734f2e4a9f93c6581692c1d7e882d2c8cf03b
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 18:52:16 +0300

    Задача 13 - решение

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N: ");
+string inputNumber = Console.ReadLine() ?? "";
+
+int numberN = int.Parse(inputNumber);
+int nThird = 3;                                  // Переменная nThird чтобы не было магических чисел
+string resDescrip;
+int result;
+
+int countN = (int)Math.Log10(numberN) + 1;
+
+if (countN<nThird)
+{
+    resDescrip = "Третьей цифры нет";
+}
+else
+{
+    int degree = (int)Math.Pow(10, countN-nThird);  // Считаем 10^(countN-nThird)
+    result = (numberN/degree)%10;                   // Делим на 10^(countN-nThird), чтобы отбросить цифры справа от третьей
+                                                    // % - чтобы получить саму третью цифру 
+    resDescrip = $"{numberN} -> {result}";
+}
+
+Console.WriteLine(resDescrip);
