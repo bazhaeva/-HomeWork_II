8174ef27fb9210b89d86e7dd704a4ce2313f96c2
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 17:59:47 +0300

    Задача 10

---
+++ task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA;
+
+//Опровобала конструкцию циклического ввода переменной из семинара
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out numberA) & numberA<999 & numberA>99)
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+
+int result = (numberA/10)%10;
+
+Console.WriteLine($"{numberA} -> {result}");
